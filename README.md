08df1f679145faf9ea56fefa996fc68df50f7bce
Vigneron <vincent.vigneron@univ-angers.fr>
2017-10-30 09:23:53 +0100

    ranger

--- README.md
+++ README.md
@@ -13,5 +13,7 @@ sudo apt-get install python3-numpy
 sudo apt-get install python3-matlib
 sudo apt-get install python3-pip
 
+sudo apt-get install ranger
+
 sudo pip3 install gTTS
 sudo apt-get install mpg321
--- install.sh
+++ install.sh
@@ -74,6 +74,22 @@ do
 	fi
 done
 
+declare -a list_of_ranger=("rc.conf")
+if [ -e $HOME/.config/ranger ]
+then
+	echo "Le répertoire .config/ranger existe déjà, je n'ai donc rien fait !"
+else
+	mkdir $HOME/.config/ranger
+fi
+for i in "${list_of_ranger[@]}"
+do
+	if [ -e $HOME/.config/ranger/$i ]
+	then
+		echo "Le fichier $i existe déjà, je n'ai donc rien fait !"
+	else
+		ln -s $directory/ranger/$i $HOME/.config/ranger/$i
+	fi
+done
 # curl https://sh.rustup.rs -sSf | sh
 # cargo install racer
 # rustup component add rust-src
