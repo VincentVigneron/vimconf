c299ac904e93aa5481db6d5fceb9c05222cbf3e9
Vigneron <vigneron.vincent.49@gmail.com>
2018-02-01 14:41:10 +0100

    link

--- link.sh
+++ link.sh
@@ -3,23 +3,41 @@
 directory="$HOME/vimconf"
 
 declare -a list_of_conf=(
-	"i3"
-	"polybar"
-	"gtk-3.0"
-	"ranger"
 	"gsimplecal"
-	"pacaur"
-	"albert"
-	"termite"
+	"gtk-3.0"
+	"i3"
 	"nvim"
+	"polybar"
+	"ranger"
+	"rofi"
+	"termite"
 )
-for i in "${list_of_conf[@]}"
-do
+for i in "${list_of_conf[@]}"; do
 	if [ -e $HOME/.config/$i ]
 	then
 		echo "Le fichier $i existe déjà, je n'ai donc rien fait !"
 	else
 		echo $i
 		ln -s $directory/$i $HOME/.config/$i
+	fi
+done
+
+declare -a list_of_conf=(
+	"bash_aliases"
+	"bash_profile"
+	"bashrc"
+	"compton.conf"
+	"ctags"
+	"dircolors"
+	"inputrc"
+	"tmux.conf"
+)
+for i in "${list_of_conf[@]}"; do
+	if [ -e $HOME/.$i ]
+	then
+		echo "Le fichier $i existe déjà, je n'ai donc rien fait !"
+	else
+		echo $i
+        ln -s $i/dircolors $HOME/.$i
 	fi
 done
